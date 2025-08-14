c2d33771dfa2f307a57c5ceb475e0f5bc294ca87
pwwang <pwwang@pwwang.com>
2025-08-14 10:44:05 -0700

    0.3.5

--- pipen_poplog.py
+++ pipen_poplog.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from pipen import Pipen, Proc
     from pipen.job import Job
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 PATTERN = r'\[PIPEN-POPLOG\]\[(?P<level>\w+?)\] (?P<message>.*)'
 logger = get_logger("poplog")
 levels = {"warn": "warning"}
